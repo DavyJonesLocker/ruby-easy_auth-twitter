293612e487b64e49c18fb76e0ab46d9f11757a48
Brian Cardarella <bcardarella@gmail.com>
2012-12-23 12:47:57 -0500

    Remove test framework opinion

--- lib/easy_auth/twitter/engine.rb
+++ lib/easy_auth/twitter/engine.rb
@@ -1,9 +1,5 @@
 module EasyAuth::Twitter
   class Engine < ::Rails::Engine
     isolate_namespace EasyAuth::Twitter
-
-    config.generators do |g|
-      g.test_framework :rspec, :view_specs => false
-    end
   end
 end
